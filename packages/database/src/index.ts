1278796f056b2dbcb336b9ecc85948454c89cf69
Mohammad <mohammad.asender95@gmail.com>
2025-10-29 16:16:53 +0200

    Update index.ts

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,3 +1,4 @@
 export * from '../prisma/generated/client'; // exports generated types from prisma
 export { prisma } from './client'; // exports instance of prisma
 export { seed } from './seed'; // exports seed function
+export * from './client'; // This brings in User, Account, etc.
