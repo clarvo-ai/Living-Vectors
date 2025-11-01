44727c45790f5ce57d192dac9d9acef62d694f89
Lasse keijonen <lasse.keijonen@aalto.fi>
2025-11-01 23:21:04 +0200

    convert .ts to .mjs for nextjs compatibility

---
+++ apps/lv-web/next.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
+};
+
+export default nextConfig;
